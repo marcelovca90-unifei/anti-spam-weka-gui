fe3937795ee2329a05994c5d9c0e84fbacdc0677
marcelovca90 <marcelovca90@gmail.com>
2016-05-13 16:31:43 -0300

    Added several classes and dependencies.

---
+++ AntiSpamEncog/src/xyz/marcelovca90/data/MessageLabel.java
@@ -0,0 +1,23 @@
+package xyz.marcelovca90.data;
+
+/**
+ * @author marcelovca90
+ *
+ */
+public enum MessageLabel {
+	
+	HAM(new Double[] { 1.0, 0.0 }), 
+	
+	SPAM(new Double[] { 0.0, 1.0 });
+	
+	private final Double[] value;
+	
+	MessageLabel(Double[] value) {
+		this.value = value; 
+	}
+
+	public Double[] getValue() {
+		return value;
+	}
+
+}
