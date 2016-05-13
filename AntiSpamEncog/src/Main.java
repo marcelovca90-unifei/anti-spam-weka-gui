34a91537d16e039465d9c8a0ace0d0e44929880c
marcelovca90 <marcelovca90@vostro>
2016-05-13 14:15:23 -0300

    Added "data" classes log4j2 configuration and converted to Maven project.

--- AntiSpamEncog/src/Main.java
+++ AntiSpamEncog/src/Main.java
@@ -1,6 +1,10 @@
-/**
- * 
- */
+import java.io.File;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import xyz.marcelovca90.data.MessageDataSet;
 
 /**
  * @author marcelovca90
@@ -8,12 +12,27 @@
  */
 public class Main {
 
+	private static final Logger logger = LogManager.getLogger(Main.class);
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		File hamFile = new File("/home/marcelovca90/Mestrado/Vectors/Ling_2016/CHI2/10/ham");
+		File spamFile = new File("/home/marcelovca90/Mestrado/Vectors/Ling_2016/CHI2/10/spam");
+		
+		MessageDataSet dataSet = new MessageDataSet(hamFile, spamFile);
+		logger.log(Level.DEBUG, dataSet);
+		
+		MessageDataSet trainingSet = dataSet.getSubset(0, 40);
+		logger.log(Level.DEBUG, trainingSet);
+		
+		MessageDataSet validationSet = dataSet.getSubset(40, 60);
+		logger.log(Level.DEBUG, validationSet);
+		
+		MessageDataSet testSet = dataSet.getSubset(60, 100);
+		logger.log(Level.DEBUG, testSet);
 	}
 
 }
