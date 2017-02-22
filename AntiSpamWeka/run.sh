04ff8c4e83979bb5a388cd6aeb5ee43ec9e2aba6
Marcelo Cysneiros <marcelovca90@gmail.com>
2017-02-22 12:05:12 -0300

    increased default stack size

--- AntiSpamWeka/run.sh
+++ AntiSpamWeka/run.sh
@@ -2,7 +2,8 @@
 
 BASEDIR=$(pwd)
 JAR_PATH="${BASEDIR}/target/AntiSpamWeka-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
-MAX_MEMORY_IN_GB=12
+MAX_HEAP_SIZE=12G
+MAX_STACK_SIZE=8m
 DATA_SET_FOLDER=${1}
 NUMBER_OF_REPETITIONS=${2}
 CSV_METHODS=(A1DE A2DE BFTREE DTNB FURIA HP J48 JRIP MLP MLPCS NBTREE RBF RF SGD SPEGASOS SVM)
@@ -11,7 +12,7 @@ if [ "${#}" -ne 2 ]; then
     echo "Illegal number of parameters. Usage: ./run.sh \"DATA_SET_FOLDER\" NUMBER_OF_REPETITIONS"
 else
     for METHOD in "${CSV_METHODS[@]}"; do
-        echo "$(date) - Started [$(basename ${JAR_PATH})] for [${METHOD}] with [${NUMBER_OF_REPETITIONS}] repetitions..."
-        java -Xmx${MAX_MEMORY_IN_GB}G -jar ${JAR_PATH} ${DATA_SET_FOLDER} ${METHOD} ${NUMBER_OF_REPETITIONS} > $(dirname ${JAR_PATH})/${METHOD}.log
+        echo "$(date) - Started [$(basename ${JAR_PATH})] with [-Xmx${MAX_HEAP_SIZE}] and [-Xss${MAX_STACK_SIZE}] for [${METHOD}] with [${NUMBER_OF_REPETITIONS}] repetitions..."
+        java -Xmx${MAX_HEAP_SIZE} -Xss${MAX_STACK_SIZE} -jar ${JAR_PATH} ${DATA_SET_FOLDER} ${METHOD} ${NUMBER_OF_REPETITIONS} > $(dirname ${JAR_PATH})/${METHOD}.log
     done
 fi
