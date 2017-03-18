4bb984a8fccc4ce270b207171df46ee02d8e3341
Marcelo Cysneiros <marcelovca90@gmail.com>
2017-03-18 12:21:27 -0300

    added evolutionary search and commons-cli

--- AntiSpamWeka/src/main/java/xyz/marcelo/helper/FilterHelper.java
+++ AntiSpamWeka/src/main/java/xyz/marcelo/helper/FilterHelper.java
@@ -1,7 +1,7 @@
 package xyz.marcelo.helper;
 
-import weka.attributeSelection.BestFirst;
 import weka.attributeSelection.CfsSubsetEval;
+import weka.attributeSelection.EvolutionarySearch;
 import weka.core.Instances;
 import weka.core.Utils;
 import weka.filters.Filter;
@@ -24,15 +24,16 @@ public class FilterHelper
     private static Instances applyAttributeFilter(Instances dataSet) throws Exception
     {
         CfsSubsetEval cfsSubsetEval = new CfsSubsetEval();
-        cfsSubsetEval.setOptions(Utils.splitOptions("-Z -P 1 -E 1"));
+        cfsSubsetEval.setOptions(Utils.splitOptions("-M -Z -P 1 -E 1"));
 
-        BestFirst bestFirst = new BestFirst();
-        bestFirst.setOptions(Utils.splitOptions("-D 2 -N 5"));
+        EvolutionarySearch evolutionarySearch = new EvolutionarySearch();
+        evolutionarySearch.setOptions(Utils.splitOptions(
+                "-population-size 20 -generations 20 -init-op 0 -selection-op 1 -crossover-op 0 -crossover-probability 0.6 -mutation-op 0 -mutation-probability 0.1 -replacement-op 0 -seed 1 -report-frequency 20"));
 
         AttributeSelection attributeSelection = new AttributeSelection();
         attributeSelection.setInputFormat(dataSet);
         attributeSelection.setEvaluator(cfsSubsetEval);
-        attributeSelection.setSearch(bestFirst);
+        attributeSelection.setSearch(evolutionarySearch);
 
         return Filter.useFilter(dataSet, attributeSelection);
     }
