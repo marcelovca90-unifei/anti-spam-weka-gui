fd40497a4f2a4809cb6fd297593dcb20ecfbbe3c
Marcelo Cysneiros <marcelovca90@gmail.com>
2017-05-13 00:41:25 -0300

    fixed test package name

--- AntiSpamWeka/src/test/java/xyz/marcelo/main/MainTest.java
+++ AntiSpamWeka/src/test/java/xyz/marcelo/main/MainTest.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  ******************************************************************************/
-package xyz.marcelo.helper.main;
+package xyz.marcelo.main;
 
 public class MainTest
 {
