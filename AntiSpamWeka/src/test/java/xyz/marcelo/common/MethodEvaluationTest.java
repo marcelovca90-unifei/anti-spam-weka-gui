caaa490c224cc22b48e4acb470be2b3ff027f7fc
Marcelo Cysneiros <marcelovca90@gmail.com>
2017-05-13 02:09:30 -0300

    implemented unit tests for MethodEvaluation

--- AntiSpamWeka/src/test/java/xyz/marcelo/common/MethodEvaluationTest.java
+++ AntiSpamWeka/src/test/java/xyz/marcelo/common/MethodEvaluationTest.java
@@ -21,7 +21,177 @@
  ******************************************************************************/
 package xyz.marcelo.common;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MethodEvaluationTest
 {
-    // TODO implement unit tests
+    @Mock
+    private Classifier classifier;
+
+    @Mock
+    private Evaluation evaluation;
+
+    private final String folder = "/some/folder/DATA_SET/STAT_METHOD/100/";
+
+    private final MethodConfiguration methodConfiguration = MethodConfiguration.RT;
+
+    private MethodEvaluation methodEvaluation;
+
+    @Before
+    public void setUp()
+    {
+        methodEvaluation = new MethodEvaluation("/some/folder/DATA_SET/STAT_METHOD/100/", MethodConfiguration.RT);
+        methodEvaluation.setClassifier(classifier);
+        methodEvaluation.setEvaluation(evaluation);
+    }
+
+    @Test
+    public void getClassifier_shouldReturnSetClassifier()
+    {
+        assertThat(methodEvaluation.getClassifier(), equalTo(classifier));
+    }
+
+    @Test
+    public void getEvaluation_shouldReturnSetEvaluation()
+    {
+        assertThat(methodEvaluation.getEvaluation(), equalTo(evaluation));
+    }
+
+    @Test
+    public void getFolder_shouldReturnSetFolder()
+    {
+        assertThat(methodEvaluation.getFolder(), equalTo(folder));
+    }
+
+    @Test
+    public void getDataSetName_shouldReturnProperDataSetName()
+    {
+        assertThat(methodEvaluation.getDataSetName(), equalTo("DATA_SET"));
+    }
+
+    @Test
+    public void getStatMethod_shouldReturnProperStatMethod()
+    {
+        assertThat(methodEvaluation.getStatMethod(), equalTo("STAT_METHOD"));
+    }
+
+    @Test
+    public void getSetNumberOfTotalFeatures_shouldReturnSetNumberOfTotalFeatures()
+    {
+        assertThat(methodEvaluation.getNumberOfTotalFeatures(), equalTo(100));
+
+        methodEvaluation.setNumberOfTotalFeatures(50);
+
+        assertThat(methodEvaluation.getNumberOfTotalFeatures(), equalTo(50));
+    }
+
+    @Test
+    public void getSetNumberOfActualFeatures_shouldReturnSetNumberOfActualFeatures()
+    {
+        assertThat(methodEvaluation.getNumberOfActualFeatures(), equalTo(100));
+
+        methodEvaluation.setNumberOfActualFeatures(50);
+
+        assertThat(methodEvaluation.getNumberOfActualFeatures(), equalTo(50));
+    }
+
+    @Test
+    public void getMethodConfiguration_shouldReturnSetMethodConfiguration()
+    {
+        assertThat(methodEvaluation.getMethodConfiguration(), equalTo(methodConfiguration));
+    }
+
+    @Test
+    public void getTrainStart_withoutTraining_shouldReturnZero()
+    {
+        assertThat(methodEvaluation.getTrainStart(), equalTo(0L));
+    }
+
+    @Test
+    public void getTrainEnd_withoutTraining_shouldReturnZero()
+    {
+        assertThat(methodEvaluation.getTrainEnd(), equalTo(0L));
+    }
+
+    @Test
+    public void getTestStart_withoutTesting_shouldReturnZero()
+    {
+        assertThat(methodEvaluation.getTestStart(), equalTo(0L));
+    }
+
+    @Test
+    public void getTestEnd_withoutTesting_shouldReturnZero()
+    {
+        assertThat(methodEvaluation.getTestEnd(), equalTo(0L));
+    }
+
+    @Test
+    public void train_whenThrowingException_shouldNotFinishTraining() throws Exception
+    {
+        doThrow(new Exception()).when(classifier).buildClassifier(any(Instances.class));
+
+        methodEvaluation.train(mock(Instances.class));
+
+        verify(classifier).buildClassifier(any(Instances.class));
+        assertThat(methodEvaluation.getTrainStart(), not(equalTo(0L)));
+        assertThat(methodEvaluation.getTrainEnd(), equalTo(0L));
+    }
+
+    @Test
+    public void train_whenNotThrowingException_shouldFinishTraining() throws Exception
+    {
+        doNothing().when(classifier).buildClassifier(any(Instances.class));
+
+        methodEvaluation.train(mock(Instances.class));
+
+        verify(classifier).buildClassifier(any(Instances.class));
+        assertThat(methodEvaluation.getTrainStart(), not(equalTo(0L)));
+        assertThat(methodEvaluation.getTrainEnd(), not(equalTo(0L)));
+        assertThat(methodEvaluation.getTrainEnd(), greaterThanOrEqualTo(methodEvaluation.getTrainStart()));
+    }
+
+    @Test
+    public void test_whenThrowingException_shouldNotFinishTesting() throws Exception
+    {
+        when(evaluation.evaluateModel(any(Classifier.class), any(Instances.class))).thenThrow(new Exception());
+
+        methodEvaluation.test(mock(Instances.class));
+
+        verify(evaluation).evaluateModel(any(Classifier.class), any(Instances.class));
+        assertThat(methodEvaluation.getTestStart(), not(equalTo(0L)));
+        assertThat(methodEvaluation.getTestEnd(), equalTo(0L));
+    }
+
+    @Test
+    public void test_whenNotThrowingException_shouldFinishTraining() throws Exception
+    {
+        when(evaluation.evaluateModel(any(Classifier.class), any(Instances.class))).thenReturn(new double[0]);
+
+        methodEvaluation.test(mock(Instances.class));
+
+        verify(evaluation).evaluateModel(any(Classifier.class), any(Instances.class));
+        assertThat(methodEvaluation.getTestStart(), not(equalTo(0L)));
+        assertThat(methodEvaluation.getTestEnd(), not(equalTo(0L)));
+        assertThat(methodEvaluation.getTestEnd(), greaterThanOrEqualTo(methodEvaluation.getTestStart()));
+    }
 }
